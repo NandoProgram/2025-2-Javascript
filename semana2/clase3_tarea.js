4ef2337823a598377c864c3dc38e18abd8a75110
NandoProgram <luisfernandolopezmorenoprogram@gmail.com>
2025-05-15 19:21:15 -0600

    update

--- semana2/clase3_tarea.js
+++ semana2/clase3_tarea.js
@@ -9,3 +9,35 @@
 // Cuenta cuántos tienen 18 años o más.
 //
 // Muestra el total.
+const { ask } = require('../helpers/input');
+
+async function main() {
+  let opcion = '';
+  let numeros = [];
+  let total=0;
+
+  while (opcion !== '3') {
+    opcion = await ask('¿Qué deseas hacer? \n1. Agregar una edad\n2. Cuantos tienen mas de 18 o 18 \n3. Salir\n');
+    if (opcion === '1') {
+      //Agregar
+      let edadNueva = await ask('¿Qué edad deseas agregar?: ');
+      numeros.push(edadNueva);
+    } else if (opcion === '2') {
+        //Salir
+        console.log('Lista de edades');
+        for(let i = 0; i < numeros.length; i++){
+            if(numeros[i]>=18){
+                total++;
+            }
+        }
+        console.log("Numero de edades mayor o igual a 18: "+ total);
+    }else if (opcion === '3') {
+      //Salir
+      console.log('Gracias por usar el programa');
+    } else {
+      console.log('Opción no válida');
+    }
+  }
+}
+
+main();
