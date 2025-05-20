e8936fbe6b00819b8f24e1bf6f36d5526b7761d7
NandoProgram <luisfernandolopezmorenoprogram@gmail.com>
2025-05-19 19:21:43 -0600

    update

--- semana2/clase4_tarea.js
+++ semana2/clase4_tarea.js
@@ -12,3 +12,45 @@
 //Promedio general
 //
 //La calificación más alta y la más baja
+
+
+function obtenerPromedio(numeros) {
+  let total = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    total = total + numeros[i];
+  }
+  const promedio = total / numeros.length;
+  return promedio;
+}
+
+function obtenerAprovdos(numeros) {
+  let aprovados = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] >= 70) {
+        aprovados++
+    }
+  }
+  return aprovados;
+}
+
+function analizarCalificaciones(numeros) {
+  const promedio = obtenerPromedio(numeros);
+  const aprovados = obtenerAprovdos(numeros);
+  const reprovados = numeros.length-aprovados;
+
+  return { aprovados, reprovados, promedio }
+}
+
+
+async function main() {
+    console.log("hola");
+  const datos = [70, 80, 90, 100, 85, 15, 20];
+  const estadistica = analizarCalificaciones(datos);
+  console.log(`El resumen estadístico de: ${datos}`);
+  console.log(`El número de aprobados es: ${estadistica.aprovados}`);
+  console.log(`El número de reprobados es: ${estadistica.reprovados}`);
+  console.log(`El promedio es: ${estadistica.promedio}`);
+
+}
+
+main();
